3bd26ae5a0e7ceb9599121f5acd3bbadd9bba5f7
Chanchanok11 <90485644+Chanchanok11@users.noreply.github.com>
2021-09-12 00:26:00 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import'./App.css';
+import CharacterCard from '.CharacterCard';
+import WordCard from './WordCard';
+const word ='Hello';
+function App() {
+    return(
+        <div>
+{
+    <WordCard value="hello" className="card"/>
+}
+    <div>
+ );
+}
+export default App;
